0f05bb39b0a45bd0695694f164ae345594848252
pedronetopgn <netopedropgn@gmail.com>
2020-04-18 22:47:16 -0300

    Curso de JavaScript - Origamid

---
+++ javascript/dom-iniciante/o-que-e-o-dom/script.js
@@ -0,0 +1,12 @@
+// Retorne a URL da página atual utilizando o objeto window
+const url = window.location.href;
+console.log(url);
+// Selecione o primeiro elemento da página que possua a classe ativo
+const ativo = document.querySelector('.ativo');
+console.log(ativo);
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+// Retorne a largura da janela
+const larguraJanela = window.innerWidth;
+console.log(larguraJanela);
