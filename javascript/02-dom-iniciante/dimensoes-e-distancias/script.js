9a4aeee6514cdbc2274cd8511280be32206289b6
pedronetopgn <netopedropgn@gmail.com>
2020-04-22 20:18:23 -0300

    Curso de JavaScript - Origamid

--- javascript/02-dom-iniciante/dimensoes-e-distancias/script.js
+++ javascript/02-dom-iniciante/dimensoes-e-distancias/script.js
@@ -1,29 +1,40 @@
-const listaAnimais = document.querySelector('.animais-lista');
+// Verifique a distância da primeira imagem em relação ao topo da página
+const img = document.querySelector('img');
+const imgTop = img.offsetTop;
+console.log(imgTop);
 
-const height = listaAnimais.clientHeight;
-const animaisTop = listaAnimais.offsetTop;
-console.log(animaisTop);
-
-const primeiroH2 = document.querySelector('h2');
-const h2left = primeiroH2.offsetLeft;
-
-const h2rect = primeiroH2.getBoundingClientRect();
-
-if(h2rect.top < 0) {
-    console.log('passou do elemento');
+// Retorne a soma da largura de todas as imagens
+function somaImagens() {
+    const imagens = document.querySelectorAll('img');
+    let soma = 0;
+    imagens.forEach((imagem) => {
+        soma += imagem.offsetWidth;
+        console.log(imagem.offsetWidth);
+    });
+    return soma;
+}
+window.onload = function () {
+    let total = somaImagens();
+    console.log('Soma da larguda das imagens',total);
 }
 
-console.log(
-    window.innerWidth,
-    window.innerHeight,
-    window.outerWidth,
-    window.outerHeight,
-    window.pageYOffset
-);
+// Verifique se os links da página possuem o tamanho mínimo recomendado para telas utilizadas com o ddo (48px/48px de acordo com o google)
+const links = document.querySelectorAll('a');
+links.forEach((link) => {
+    const linkWidth = link.offsetWidth;
+    const linkHeight = link.offsetHeight;
+    if(linkWidth >= 48 && linkHeight >= 48) {
+        console.log(link, 'Possui acessibilidade');
+    } else {
+        console.log(link, 'Não possui boa acessibilidade');
 
-const small = window.matchMedia('(max-width: 600px)').matches; // verifica o width atual da página, responsividade.
-if(small) {
-    console.log('Usuário mobile');
-} else {
-    console.log('Usuário desktop')
+    }
+});
+
+// Se o browser for menor que 720px, adicione a classe menu-mobile ao menu
+const browserSmall = window.matchMedia('(max-width: 720px)').matches;
+
+if(browserSmall) {
+    const menu = document.querySelector('.menu');
+    menu.classList.add('menu-mobile');
 }
