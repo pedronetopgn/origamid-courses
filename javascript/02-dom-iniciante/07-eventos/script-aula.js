bbabf755ba67b442ebc912f98f11cf8565b05f94
pedronetopgn <netopedropgn@gmail.com>
2020-04-22 23:48:47 -0300

    Curso de JavaScript - Origamid

---
+++ javascript/02-dom-iniciante/07-eventos/script-aula.js
@@ -0,0 +1,57 @@
+const img = document.querySelector('img');
+
+function callback(event) {
+    console.log(event);
+}
+
+// img.addEventListener('click', callback); 
+
+// const animaisLista = document.querySelector('.animais-lista');
+
+function callbackLista(event) {
+    console.log(event.currentTarget); // informação geral
+    console.log(event.target); // onde o clique ocorreu 
+    console.log(event.type); // tipo do evento
+}
+
+// animaisLista.addEventListener('click', callbackLista);
+
+const linkExterno = document.querySelector('a[href^="http"]');
+
+function handleLinkExterno(event) {
+    event.preventDefault(); // previne a ação que ocorreria
+    console.log(this.getAttribute('href')); // fala com o elemento linkExterno = event.currentTarget
+}
+
+linkExterno.addEventListener('click', handleLinkExterno);
+
+const h1 = document.querySelector('h1');
+
+function handleEvent(event) {
+    console.log(event.type, event);
+}
+
+// h1.addEventListener('click', handleEvent);
+// h1.addEventListener('mouseenter', handleEvent);
+// h1.addEventListener('mousemove', handleEvent);
+
+// window.addEventListener('scroll', handleEvent);
+// window.addEventListener('resize', handleEvent);
+
+function handleKeyboard(event) {
+    if(event.key === 'a') {
+        document.body.classList.toggle('azul');
+    }
+}
+
+window.addEventListener('keydown', handleKeyboard);
+
+const imgs = document.querySelectorAll('img');
+
+function handleImg(event) {
+    console.log(event.target.getAttribute('src'));
+}
+
+imgs.forEach((img) => {
+    img.addEventListener('click', handleImg);
+});
