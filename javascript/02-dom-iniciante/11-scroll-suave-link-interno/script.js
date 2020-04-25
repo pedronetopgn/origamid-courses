d38e6e00bece5711c28b7cc4969748dc6ba28173
pedronetopgn <netopedropgn@gmail.com>
2020-04-24 22:20:11 -0300

    Curso de JavaScript - Origamid

---
+++ javascript/02-dom-iniciante/11-scroll-suave-link-interno/script.js
@@ -0,0 +1,66 @@
+function initTabNav(){
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+    tabContent[0].classList.add('ativo');
+
+    if(tabMenu.length && tabContent.length){ //verificando se está selecionando
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabContent[index].classList.add('ativo');
+        }
+        
+        tabMenu.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                activeTab(index);
+            });
+        });
+    }
+}
+initTabNav();
+
+function initAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo'
+    
+    if(accordionList.length){
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass); // this = event.currentTarget
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+initAccordion();
+
+function initScrollSuave(){
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({ // sem precisar verificar offsetTop
+            behavior: 'smooth',
+            block: 'start',
+        });
+
+        // window.scrollTo({ // animação suave, forma alternativa
+        //     top: topo,
+        //     behavior: 'smooth',
+        // });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+}
+initScrollSuave();
