6a7b3ad06c5715ee4052a8c9a94a1936318cf4f9
pedronetopgn <netopedropgn@gmail.com>
2020-05-02 21:40:32 -0300

    Curso de JavaScript - Origamid

---
+++ javascript/03-objetos/09-object/script-aula.js
@@ -0,0 +1,92 @@
+// const carro = {
+//     rodas: 4,
+//     init(valor) {
+//         this.marca = valor;
+//         return this;
+//     },
+//     acelerar() {
+//         return this.marca + ' acelerou';
+//     },
+//     buzinar() {
+//         return this.marca + ' buzinou';
+
+//     }
+// }
+
+// const honda = Object.create(carro).init('Honda');
+// console.log(honda.acelerar())
+
+// const ferrari = Object.create(carro).init('Ferrari');
+// console.log(ferrari.acelerar())
+
+// const funcaoAutomovel = {
+//     acelerar() {
+//     return 'acelerou';
+//     },
+//     buzinar() {
+//     return 'buzinou';
+//     },
+// }
+
+// const moto = {
+//     rodas: 2,
+//     capacete: true,
+// }
+
+// Object.assign(moto, funcaoAutomovel); // adiciona métodos de funcaoAutomovel em moto
+// console.log(moto);
+
+const moto = {
+}
+
+
+Object.defineProperties(moto, { // dessa forma, torna a propriedade constante
+    rodas: {
+        enumerable: true,
+        //value: 2, // não pode ter valor quando tem get e set
+        //configurable: false, // se permite deletar
+        //writable: false, // se permite alteração
+        get() {
+            return this._rodas;
+        },
+        set(valor) {
+            this._rodas = valor * 4;
+        }
+    }
+});
+moto.rodas = 5;
+console.log(moto);
+
+Object.keys(Array);
+// [] vazia, pois não possui propriedades enumeráveis
+const carro = {
+    marca: 'Ford',
+    ano: 2018,
+}
+Object.keys(carro);
+// ['marca', 'ano']
+Object.values(carro);
+// ['Ford', 2018]
+Object.entries(carro);
+// [['marca', 'Ford'], ['ano', 2018]]
+
+let frutas = ['Banana'];
+console.log(Object.getPrototypeOf(frutas)); // protótipo de array
+let frutas2 = ['Banana'];
+// frutas2 = frutas; tornaria true
+console.log(Object.is(frutas,frutas2));
+
+
+// Object.freeze() impede qualquer mudança nas propriedades.
+// Object.seal() previne a adição de novas propriedades e
+// impede que as atuais sejam deletadas.
+// Object.preventExtensions() previne a adição de novas
+// propriedades.
+Object.freeze(carro);
+Object.seal(carro);
+Object.preventExtensions(carro);
+Object.isFrozen(carro);
+
+
+
+console.log(Object.prototype.toString.call(frutas)); // como verificar o tipo de objeto q está lidando
--- javascript/03-objetos/09-object/script.js
+++ javascript/03-objetos/09-object/script.js
@@ -1,86 +1,32 @@
-// const carro = {
-//     rodas: 4,
-//     init(valor) {
-//         this.marca = valor;
-//         return this;
-//     },
-//     acelerar() {
-//         return this.marca + ' acelerou';
-//     },
-//     buzinar() {
-//         return this.marca + ' buzinou';
-
-//     }
-// }
-
-// const honda = Object.create(carro).init('Honda');
-// console.log(honda.acelerar())
-
-// const ferrari = Object.create(carro).init('Ferrari');
-// console.log(ferrari.acelerar())
-
-// const funcaoAutomovel = {
-//     acelerar() {
-//     return 'acelerou';
-//     },
-//     buzinar() {
-//     return 'buzinou';
-//     },
-// }
-
-// const moto = {
-//     rodas: 2,
-//     capacete: true,
-// }
-
-// Object.assign(moto, funcaoAutomovel); // adiciona métodos de funcaoAutomovel em moto
-// console.log(moto);
-
-const moto = {
+// Crie uma função que verifique
+// corretamente o tipo de dado
+fruta = ['banana'];
+function verificaTipoDado(dado){
+    return Object.prototype.toString.call(dado);
 }
-
-
-Object.defineProperties(moto, { // dessa forma, torna a propriedade constante
-    rodas: {
+console.log(verificaTipoDado(fruta));
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
+const quadrado = {}
+Object.defineProperties(quadrado, {
+    lados: {
+        value: 4,
         enumerable: true,
-        //value: 2, // não pode ter valor quando tem get e set
-        //configurable: false, // se permite deletar
-        //writable: false, // se permite alteração
-        get() {
-            return this._rodas;
-        },
-        set(valor) {
-            this._rodas = valor * 4;
-        }
     }
-});
-moto.rodas = 5;
-console.log(moto);
+})
 
-Object.keys(Array);
-// [] vazia, pois não possui propriedades enumeráveis
-// const carro = {
-//     marca: 'Ford',
-//     ano: 2018,
-// }
-Object.keys(carro);
-// ['marca', 'ano']
-Object.values(carro);
-// ['Ford', 2018]
-Object.entries(carro);
-// [['marca', 'Ford'], ['ano', 2018]]
-
-let frutas = ['Banana'];
-console.log(Object.getPrototypeOf(frutas)); // protótipo de array
-let frutas2 = ['Banana'];
-// frutas2 = frutas; tornaria true
-console.log(Object.is(frutas,frutas2));
-
-const carro = {
-    marca: 'Ford',
-    ano: 2018,
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+    width: 800,
+    height: 600,
+    background: '#333'
 }
-    Object.freeze(carro);
-    Object.seal(carro);
-    Object.preventExtensions(carro);
-    Object.isFrozen(carro);
+Object.freeze(configuracao);
+
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
+console.log(Object.getOwnPropertyNames(String.prototype));
+console.log(Object.getOwnPropertyNames(Array.prototype));
